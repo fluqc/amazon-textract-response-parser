1ba357758537c036ab877406048554b962364570
Benjamin Hamilton-Olsen <bentho@gmail.com>
2025-07-30 13:03:18 +0100

    Move dir down

--- src-js/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  // `examples` packages define their own test commands
-  testPathIgnorePatterns: ["/examples/", "/node_modules/"]
-};
